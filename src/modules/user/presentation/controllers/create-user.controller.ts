8b99666d01be62cc8c86fac2f28281241b83533b
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2024-08-31 02:00:53 -0300

    chore: remove snowflake to use ulid

--- src/modules/user/presentation/controllers/create-user.controller.ts
+++ src/modules/user/presentation/controllers/create-user.controller.ts
@@ -1,11 +1,10 @@
 import { Controller, Post } from '@nestjs/common'
-import { SnowflakeGeneratorService } from '@shared/utils/helpers/snow-flake.service'
 
 @Controller('user')
 export class CreateUserController {
-  constructor(private id: SnowflakeGeneratorService) {}
+  constructor() {}
   @Post()
   async createUser() {
-    return this.id.generateID()
+    return 'create'
   }
 }
--- src/modules/user/presentation/user.module.ts
+++ src/modules/user/presentation/user.module.ts
@@ -1,12 +1,11 @@
 import { Module, Type } from '@nestjs/common'
 import { PrismaService } from '@shared/libs/prisma.service'
-import { SnowflakeGeneratorService } from '@shared/utils/helpers/snow-flake.service'
 import userControllers from './controllers'
 
 const _userControllers: Type<any>[] = Object.values(userControllers)
 
 @Module({
   controllers: [..._userControllers],
-  providers: [PrismaService, SnowflakeGeneratorService]
+  providers: [PrismaService]
 })
 export class UserModule {}
