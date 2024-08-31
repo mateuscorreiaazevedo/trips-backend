e957b369cd992a2cf1434519ac1f80d9f58499b7
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2024-08-30 22:45:12 -0300

    chore: setup app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { AppModule } from '@shared/app/app.module'
+import env from '@shared/utils/configs/env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  await app.listen(env.PORT)
+  console.log(`🚀 Server is running on port ${env.PORT}`)
 }
 bootstrap()
